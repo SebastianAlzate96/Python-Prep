b3eac6cd49830a39e38bba55b3c8ecbe33a880e2
Sebastian Alzate Vargas <sebas.mate52@gmail.com>
2023-03-13 21:03:57 -0400

    Primera tarea lista

---
+++ 02 - Variables y Tipos de Datos/Solution_Homework_02.py
@@ -0,0 +1,87 @@
+"1) Crear una variable que contenga un elemento del conjunto de números enteros y luego imprimir por pantalla"
+entero = 5
+
+"2) Imprimir el tipo de dato de la constante 8.5"
+print(8.5)
+
+"3) Imprimir el tipo de dato de la variable creada en el punto 1"
+print(type(entero))
+
+"4) Crear una variable que contenga tu nombre"
+name = "Sebastian Alzate Vargas"
+
+"5) Crear una variable que contenga un número complejo"
+var1_complex = 5-1j
+
+"6) Mostrar el tipo de dato de la variable crada en el punto 5"
+type(var1_complex)
+
+"7) Crear una variable que contenga el valor del número Pi redondeado a 4 decimales"
+pi = 3.1416
+
+"8) Crear una variable que contenga el valor 'True' y otra que contenga el valor True. ¿Se trata de lo mismo?"
+var_1 = 'True'
+var_2 = True 
+# no se trata de lo mismo ya la primera es un caracter y la segunda un booleano
+
+"9) Imprimir el tipo de dato correspondientes a las variables creadas en el punto 9"
+print(type(var_1),type(var_2))
+
+"10) Asignar a una variable, la suma de un número entero y otro decimal"
+suma_entero = 5+5.1
+
+"11) Realizar una operación de suma de números complejos"
+var2_complex = 6+2j
+var1_complex +var2_complex
+
+"12) Realizar una operación de suma de un número real y otro complejo"
+5.2+var2_complex
+
+"13) Realizar una operación de multiplicación"
+5*3
+
+"14) Mostrar el resultado de elevar 2 a la octava potencia"
+2**8
+
+"15) Obtener el cociente de la división de 27 entre 4 en una variable y luego mostrarla"
+print(27/4)
+
+"16) De la división anterior solamente mostrar la parte entera"
+print(27//4)
+
+"17) De la división de 27 entre 4 mostrar solamente el resto"
+print(27%4)
+
+"18) Utilizando como operandos el número 4 y los resultados obtenidos en los puntos 16 y 17. Obtener 27 como resultado"
+
+"19) Utilizar el operador + en una operación donde intervengan solo variables alfanuméricas"
+print("hola " + " mundo")
+
+"20) Evaluar si 2 es igual a 2. ¿Por qué ocurre eso?"
+"2" == 2
+
+"21) Utilizar las funciones de cambio de tipo de dato, para que la validación del punto 20 resulte verdadera"
+"2" == str(2)
+int == 2
+
+"22) ¿Por qué arroja error el siguiente cambio de tipo de datos? a = float('3,8')"
+#por la coma
+
+"23) Crear una variable con el valor 3, y utilizar el operador '-=' para modificar su contenido"
+x=3
+x-=2
+print(x)
+
+"24) Realizar la operacion 1 << 2 ¿Por qué da ese resultado? ¿Qué es el sistema de numeración binario?"
+print(1<<2)
+
+"25) Realizar la operación 2 + '2' ¿Por qué no está permitido? ¿Si los dos operandos serían del mismo tipo, siempre arrojaría el mismo resultado?"
+#no
+
+# In[27]:
+"26) Realizar una operación válida entre valores de tipo entero y string"
+var1 = 'este texto se repite '
+var2 = 3
+print(var1 * var2 + str(var2) + ' veces')
+
+# %%
